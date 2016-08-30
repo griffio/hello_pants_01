a5c678514adae15d951f709844e1a308421acadd
griffio <griffio@users.noreply.github.com>
2016-08-30 15:29:42 +0100

    project files
    Pants build

---
+++ src/test/java/griffio/Tests.java
@@ -0,0 +1,13 @@
+package griffio;
+
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class Tests {
+
+				@Test
+				public void expect_b() {
+				     assertThat("abc").contains("b");
+				}
+}
